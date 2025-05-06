ba6b6b9c01729d99ff31089a47b899f30d360956
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-05-06 14:57:54 +0500

    first com

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Get, Controller, Redirect } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
+@Controller()
+export class AppController {
+  @Get()
+  @Redirect('/docs')
+  @ApiExcludeEndpoint()
+  redirectToSwagger() {
+    return;
+  }
+
+  @Get('/hello')
+  hello() {
+    return 'Hello developer4 Ilya';
+  }
+}
